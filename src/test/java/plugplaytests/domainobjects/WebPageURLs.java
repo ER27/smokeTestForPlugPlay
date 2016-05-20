661f8b0df3b0673e1835f6c383d73cce69a115d3
ER27 <rajmund.nowicki@gmail.com>
2016-05-20 15:00:55 +0200

    first commit and push

---
+++ src/test/java/plugplaytests/domainobjects/WebPageURLs.java
@@ -0,0 +1,13 @@
+package plugplaytests.domainobjects;
+
+/**
+ * Created by ER27 on 2016-05-20.
+ */
+public class WebPageURLs {
+
+    public static final String MAIN_URL = "http://plug-play.pl/";
+
+/*        TODO
+Add URLs to: 'SALA 1', 'ALA 2', 'SALA A', 'SALA B' etc.
+*/
+}
---
+++ src/test/java/plugplaytests/smoke/TestIfWebPageIsVisible.java
@@ -0,0 +1,50 @@
+package plugplaytests.smoke;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static plugplaytests.domainobjects.WebPageContent.BOOKING;
+import static plugplaytests.domainobjects.WebPageContent.PLUGPLAY_TITLE;
+import static plugplaytests.domainobjects.WebPageContent.PRICING;
+import static plugplaytests.domainobjects.WebPageURLs.MAIN_URL;
+
+/**
+ * Created by ER27 on 2016-05-20.
+ */
+public class TestIfWebPageIsVisible {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public void openPlugPlayWebSiteViaFireFox() {
+
+        driver = new FirefoxDriver();
+
+        driver.navigate().to(MAIN_URL);
+        driver.manage().window().maximize();
+
+        WebDriverWait wait = new WebDriverWait(driver, 1);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(PLUGPLAY_TITLE)));
+    }
+
+    @AfterClass
+    public void closePlugPlayWebSite() {
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public void checkIfBookingAndPricingButtonsAreClickable() {
+
+        WebDriverWait wait = new WebDriverWait(driver, 1);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(BOOKING)));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(PRICING)));
+
+    }
+}
