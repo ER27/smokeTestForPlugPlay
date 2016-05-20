d1ddfdf900fa4a15b40390d73bc91dd35e3a1b8d
ER27 <rajmund.nowicki@gmail.com>
2016-05-20 14:58:47 +0200

    add .idea folder to git ignore

---
+++ src/test/java/plugplaytests/domainobjects/WebPageContent.java
@@ -0,0 +1,11 @@
+package plugplaytests.domainobjects;
+
+/**
+ * Created by ER27 on 2016-05-20.
+ */
+public class WebPageContent {
+
+    public static final String PLUGPLAY_TITLE = ".//img[contains(@alt,'PLUG&PLAY')]";
+    public static final String BOOKING = ".//a[contains(@class,'red-btn')]";
+    public static final String PRICING = ".//a[contains(@class,'green-btn')]";
+}
